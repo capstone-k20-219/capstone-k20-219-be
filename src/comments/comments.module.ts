3e0801296f92ecf15091ce63318d1e66495ddaa7
CT08\CT08 <pcthanh0802@gmail.com>
2024-04-17 18:56:27 +0700

    fix: add mongoose import to comment, parking slot and vehicle modules

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentEntity, CommentSchema } from './entities/comment.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: CommentEntity.name, schema: CommentSchema },
+    ]),
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- src/parking-slots/parking-slots.module.ts
+++ src/parking-slots/parking-slots.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ParkingSlotsService } from './parking-slots.service';
 import { ParkingSlotsController } from './parking-slots.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ParkingSlotEntity,
+  ParkingSlotSchema,
+} from './entities/parking-slot.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ParkingSlotEntity.name, schema: ParkingSlotSchema },
+    ]),
+  ],
   controllers: [ParkingSlotsController],
   providers: [ParkingSlotsService],
 })
