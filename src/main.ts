ef11c963fc70ac7a8b219fe5be44c0613bf27236
CT08\CT08 <pcthanh0802@gmail.com>
2024-05-13 01:10:42 +0700

    chore: config for deployment

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Pakislot - Back-end server')
